87df257c5bd85cca49f44e7b79639de94487c12a
Diego <dcoaguila@gmail.com>
2020-06-24 21:45:46 -0700

    refactor: Rename color settings variables

--- script.js
+++ script.js
@@ -1,43 +1,126 @@
-//generateInitialGrid() creates an initial 16x16 div grid
-function generateDivGrid () {
-  let i;
-  for (i = 0; i < 256; i++) {
-    let div = document.createElement("div");
+// Color settings functions
+function blackBackground(e) {
+  e.target.style.backgroundColor = "black";
+}
+
+function randomBackground(e) {
+  const randomColor = Math.floor(Math.random() * 16777215).toString(16);
+  e.target.style.backgroundColor = `+${randomColor}`;
+}
+
+function shadeBackground(e) {
+  const { target } = e;
+  switch (target.style.backgroundColor) {
+    case "":
+      e.target.style.backgroundColor = "#cccccc";
+      break;
+    case "rgb(204, 204, 204)":
+      e.target.style.backgroundColor = "#999999";
+      break;
+    case "rgb(153, 153, 153)":
+      e.target.style.backgroundColor = "#666666";
+      break;
+    case "rgb(102, 102, 102)":
+      e.target.style.backgroundColor = "#323232";
+      break;
+    case "rgb(50, 50, 50)":
+      e.target.style.backgroundColor = "#000000";
+      break;
+    default:
+  }
+}
+
+function updateEventListener(e) {
+  const eventHandlerName = e.target.value;
+  const gridDivs = document.querySelectorAll(".grid-item");
+  switch (eventHandlerName) {
+    case "black":
+      gridDivs.forEach((div) => {
+        div.removeEventListener("mouseover", randomBackground);
+        div.removeEventListener("mouseover", shadeBackground);
+        div.addEventListener("mouseover", blackBackground);
+      });
+      break;
+    case "random":
+      gridDivs.forEach((div) => {
+        div.removeEventListener("mouseover", blackBackground);
+        div.removeEventListener("mouseover", shadeBackground);
+        div.addEventListener("mouseover", randomBackground);
+      });
+      break;
+    case "shade":
+      gridDivs.forEach((div) => {
+        div.removeEventListener("mouseover", blackBackground);
+        div.removeEventListener("mouseover", randomBackground);
+        div.addEventListener("mouseover", shadeBackground);
+      });
+      break;
+    default:
+  }
+}
+
+// Grid functions
+function renderInitialGridDivs() {
+  for (let i = 0; i < 256; i += 1) {
+    const div = document.createElement("div");
     div.setAttribute("class", "grid-item");
     document.getElementById("grid-container").appendChild(div);
-  let initialDivs =  document.querySelectorAll(".grid-item")
-  initialDivs.forEach(function(element) {
-    element.addEventListener("mouseover", function() {
-    element.style.backgroundColor = "black";
-      }); 
+    const initialDivs = document.querySelectorAll(".grid-item");
+    initialDivs.forEach((element) => {
+      element.addEventListener("mouseover", blackBackground);
     });
   }
 }
-generateDivGrid();
 
-let button = document.getElementById("button");
-button.addEventListener("click", function(){
-  let input = prompt("Number of Squares: ");
-  resetGrid(input);
-});
-function resetGrid (input) {
-  let userInput = input;
-  usertInput = parseInt(userInput);
-  height = 80/userInput;
-  var i;
-  let userSquares = input*input;
-  document.querySelectorAll('.grid-item').forEach(el => el.remove());
-  for (i = 0; i < userSquares; i++) {
-    let div = document.createElement("div");
+function eraseGrid() {
+  const gridDivs = document.querySelectorAll(".grid-item");
+  gridDivs.forEach((div) => {
+    div.style.backgroundColor = "";
+  });
+}
+
+function setNewGrid(input) {
+  if (input === null) return;
+  input = parseInt(input);
+  const userSquares = input * input;
+  document.querySelectorAll(".grid-item").forEach((el) => el.remove());
+  for (let i = 0; i < userSquares; i += 1) {
+    const div = document.createElement("div");
     div.setAttribute("class", "grid-item");
     document.getElementById("grid-container").appendChild(div);
   }
-  document.getElementById("grid-container").style.gridTemplateColumns = `repeat(${userInput}, auto)`;
-  document.getElementById("grid-container").style.gridTemplateRows = `repeat(${userInput}, ${height}vh)`;
-  let divs =  document.querySelectorAll(".grid-item")
-divs.forEach(function(element) {
-  element.addEventListener("mouseover", function() {
-    element.style.backgroundColor = "black";
-  }); 
-});
+  document.getElementById(
+    "grid-container"
+  ).style.gridTemplateColumns = `repeat(${input}, auto)`;
+  document.getElementById(
+    "grid-container"
+  ).style.gridTemplateRows = `repeat(${input}, auto`;
+  const divs = document.querySelectorAll(".grid-item");
+  divs.forEach((element) => {
+    element.addEventListener("mouseover", blackBackground);
+  });
 }
+
+function recieveUserInput() {
+  const userInput = prompt("Number of Squares: ");
+  setNewGrid(userInput);
+}
+
+// DOM methods
+const newGridButton = document.getElementById("new-grid");
+newGridButton.addEventListener("click", recieveUserInput);
+
+const eraseGridButton = document.getElementById("erase-grid");
+eraseGridButton.addEventListener("click", eraseGrid);
+
+const randomColorButton = document.getElementById("random-color-button");
+randomColorButton.addEventListener("click", updateEventListener);
+
+const blackColorButton = document.getElementById("black-color-button");
+blackColorButton.addEventListener("click", updateEventListener);
+
+const shadeInButton = document.getElementById("shade-in-button");
+shadeInButton.addEventListener("click", updateEventListener);
+
+// Render initial grid
+renderInitialGridDivs();
